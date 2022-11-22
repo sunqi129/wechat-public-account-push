d48cda5f84661e293afa5f663f95159922c6daeb
sunqi129 <49363468+sunqi129@users.noreply.github.com>
2022-11-22 17:11:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -44,7 +44,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '0001',
+  CALLBACK_TEMPLATE_ID: 'ExAFgrJHejzz_iUHNFRVOsY-neQ5Vm_-r9nmdQgUlXU',
 
   CALLBACK_USERS: [
     {
