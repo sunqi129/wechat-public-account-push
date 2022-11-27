0b820318bc06566d47589b5bb96d3bdb8594a067
sunqi129 <49363468+sunqi129@users.noreply.github.com>
2022-11-27 11:18:35 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -31,7 +31,7 @@ USE_PASSAGE: 'push-deer',
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2017-12-16' },
+        { keyword: 'love_day', date: '2015-12-16' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2019-09-16' },
       ],
